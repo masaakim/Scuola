4c0d833824ac584de5e8694249d69043997ecf96
Masaaki Morishita <ikasama3.0@gmail.com>
2014-02-10 03:17:32 +0900

    Set Gruntfile

---
+++ Gruntfile.js
@@ -0,0 +1,113 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+
+    sass: {
+      dist: {
+        files: {
+          'build/scuola.css': 'scss/scuola.scss'
+        }
+      }
+    },
+ 
+    styleguide: {
+      styledocco: {
+        options: {
+          name: 'Project Name',
+          framework: {
+            name: 'styledocco',
+            options: {
+              preprocessor: 'sass'
+            }
+          },
+          template: {
+            include: [
+              '_previews.js'
+            ]
+          }
+        },
+        src: [
+          'scss/*.scss'
+        ],
+        dest: 'docs'
+      }
+    },
+
+    clean: {
+      styleguide: [
+        '<%= styleguide.styledocco.dest %>'
+      ]
+    },
+
+    connect: {
+      app: {
+        options: {
+          port: 2020,
+          base: 'build/',
+          open: 'http://localhost:2020'
+        }
+      },
+      doc: {
+        options: {
+          port: 2021,
+          base: 'build/',
+          open: 'http://localhost:2021'
+        }
+      }
+    },
+
+    csscomb: {
+      dist: {
+        options: {
+          config: '.csscombrc'
+        },
+        files: {
+          'build/scuola.css': ['build/scuola.css']
+        }
+      }
+    },
+
+    csslint: {
+      dist: {
+        options: {
+          csslintrc: '.csslintrc'
+        }
+      },
+      src: ['build/scuola.css']
+    },
+
+
+    watch: {
+      options: {
+        livereload: true
+      },
+      stylesheets: {
+        files: ['scss/*.scss'],
+        tasks: ['compass']
+      },
+    },
+
+  });
+
+  grunt.registerTask('default', [
+    'clean',
+    'styleguide'
+  ]);
+
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-compass');
+  grunt.loadNpmTasks('grunt-styleguide');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-csslint');
+  grunt.loadNpmTasks('grunt-contrib-csslint');
+  grunt.loadNpmTasks('grunt-csscss');
+  grunt.loadNpmTasks('grunt-csscomb');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+
+  grunt.registerTask('default', ['develop']);
+  grunt.registerTask('develop', ['connect:app', 'watch']);
+  grunt.registerTask('stylesheet', ['sass', 'csslint', 'csscomb']);
+  grunt.registerTask('publish', ['stylesheet', 'clean', 'styleguide', 'connect:doc', 'watch']);
+
+};
